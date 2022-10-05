03b049313f86f05e74bf1cd527a3407409918629
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-10-05 14:48:07 -0300

    open e closed
    coloquei script

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('#open-sideBar')
+
+button.addEventListener('click', closed)
+function closed() {
+  const closed = document.querySelector()
+  closed.classList.remove('closed')
+};
+
+const body = document.querySelector('body')
+body.addEventListener('keydown', open)
+function open(event) {
+  var tecla = event.keyCode;
+  if(tecla === 27){
+    const open = document.querySelector()
+  }
+}
