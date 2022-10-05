42008336f09e43c4c407a70f4f35e2209f65adc8
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-10-05 14:59:09 -0300

    inicio novamente do script

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const button = document.querySelector('#open-sideBar')
+const button = document.querySelector('#open-sideBar');
+const sideBar = document.querySelector()
 
-button.addEventListener('click', closed)
-function closed() {
-  const closed = document.querySelector()
-  closed.classList.remove('closed')
-};
-
-const body = document.querySelector('body')
-body.addEventListener('keydown', open)
-function open(event) {
-  var tecla = event.keyCode;
-  if(tecla === 27){
-    const open = document.querySelector()
-  }
-}
