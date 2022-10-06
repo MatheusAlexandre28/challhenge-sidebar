2c233869eb779ac58c20d21496b75442df740c86
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-10-06 15:11:59 -0300

    Fiz um script e configurei ele
    Primeiro coloquei as classes sidebar, open-sidebar e search q é a barra de pesquisa. Peguei meu btn q no caso é o opensideBar, e coloquei .onclick, q é para quando eu clicar ele faça uma function, no caso peguei essa function e coloquei a nossa sideBar com um .classList com um .toggle() dentro um active, q é quando eu pegar no meu css para configurar o sideBar ele vai ativar este CSS configurado, e quando eu clicar novamente ele vai desativar.
     Temos o exemplo no nosso CSS pegamos o .sideBar com o .active q no caso é quando clicar q vai ativar ele executa o CSS abaixo.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const button = document.querySelector('#open-sideBar');
-const sideBar = document.querySelector()
+const btn = document.querySelector('#open-sideBar');
+const sideBar = document.querySelector('.sideBar');
+const searchBtn = document.querySelector('.search');
+
+btn.onclick = function() {
+  sideBar.classList.toggle("active");
+}
+
+searchBtn.onclick = function() {
+  sideBar.classList.toggle("active");
+}
+
+
 
